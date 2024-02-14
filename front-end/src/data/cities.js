149ec88f253e127e29f52961a767cdb80a5b27f3
Joshua Yu <joshty1215@gmail.com>
2024-02-14 12:42:40 -0600

    placeholder splash page

--- front-end/src/data/cities.js
+++ front-end/src/data/cities.js
@@ -1,38 +1,42 @@
+import austin from "../assets/austin.png";
+import sanfran from "../assets/sanfran.png";
+import miami from "../assets/miami.png";
+
 export const citiesData = [
-    {
-        // img_src: "path_to_image_city1.jpg",
-        name: "Austin",
-        id: 1,
-        population: "964,177",
-        numSchools: 125,
-        testScore: "1150",
-        medianIncome: "$45,000",
-        percentUnemployment: 4,
-        freeLunch: 35,
-        educated: 55
-    },
-    {
-        // img_src: "path_to_image_city2.jpg",
-        name: "San Francisco",
-        id: 2,
-        population: "815,201",
-        numSchools: 113,
-        testScore: "1100",
-        medianIncome: "$40,000",
-        percentUnemployment: 6,
-        freeLunch: 40,
-        educated: 50
-    },
-    {
-        // img_src: "path_to_image_city3.jpg",
-        name: "Miami",
-        id: 3,
-        population: "439,890",
-        numSchools: 522,
-        testScore: "1180",
-        medianIncome: "$48,000",
-        percentUnemployment: 5,
-        freeLunch: 32,
-        educated: 58
-    }
+  {
+    img_src: austin,
+    name: "Austin",
+    id: 1,
+    population: "964,177",
+    numSchools: 125,
+    testScore: "1150",
+    medianIncome: "$45,000",
+    percentUnemployment: 4,
+    freeLunch: 35,
+    educated: 55,
+  },
+  {
+    img_src: sanfran,
+    name: "San Francisco",
+    id: 2,
+    population: "815,201",
+    numSchools: 113,
+    testScore: "1100",
+    medianIncome: "$40,000",
+    percentUnemployment: 6,
+    freeLunch: 40,
+    educated: 50,
+  },
+  {
+    img_src: miami,
+    name: "Miami",
+    id: 3,
+    population: "439,890",
+    numSchools: 522,
+    testScore: "1180",
+    medianIncome: "$48,000",
+    percentUnemployment: 5,
+    freeLunch: 32,
+    educated: 58,
+  },
 ];
