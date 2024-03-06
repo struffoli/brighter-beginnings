b48abc8636edc04cdcd7d311b676b1eea3ee8d0a
Kevin Xu <kxu@cs.utexas.edu>
2024-03-06 17:50:14 -0600

    USE BACKEND API FROM FRONTEND

--- front-end/src/pages/Cities.jsx
+++ front-end/src/pages/Cities.jsx
@@ -1,18 +1,51 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 import CitiesCard from "../components/cards/CitiesCard";
-import { cities } from "../data/cities.js"; // Importing cities data
+// vvvvvvvvvvvvvvvvvvvvvvvvvvvv this should no longer be needed !!!!!!!!
+// import { cities } from "../data/cities.js"; // Importing cities data
 import mapPage from "../assets/maptemp.png";
 import Pagination from "../components/Pagination.jsx";
 import "./Cities.css"
 
+async function getCities() {
+  let cities = [];
+  let total_cities = 0;
+  try {
+    const response = await fetch("https://api.brighterbeginnings.me/cities");
+    const result = await response.json();
+    cities = result["Cities"];
+    total_cities = result["Total cities"];
+  } catch (error) {
+    console.log(error);
+  }
+  return { cities, total_cities }
+}
+
+export async function getCityById(id) {
+  let city = {};
+  try {
+    const response = await fetch(`https://api.brighterbeginnings.me/cities/${id}`);
+    city = await response.json();
+  } catch (error) {
+    console.log(error);
+  }
+  return city;
+}
+
 const Cities = () => {
   const [currentPage, setCurrentPage] = useState(1);
-  const [itemsPerPage] = useState(3); // Set the number of items per page
+  const [itemsPerPage] = useState(15); // Set the number of items per page
+
+  // Get cities from API
+  const [apiCities, setApiCities] = useState({ cities: [], total_cities: 0 });
+  useEffect(() => {
+    getCities().then((data) => setApiCities(data));
+  }, []);
 
   // Get current items
   const indexOfLastItem = currentPage * itemsPerPage;
   const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-  const currentItems = cities.slice(indexOfFirstItem, indexOfLastItem);
+  // const currentItems = cities.slice(indexOfFirstItem, indexOfLastItem);
+  const currentItems = apiCities.cities.slice(indexOfFirstItem, indexOfLastItem);
 
   // Change page
   const paginate = pageNumber => setCurrentPage(pageNumber);
@@ -31,8 +64,10 @@ const Cities = () => {
       </div>
       <div className="row justify-content-center mb-5 mx-4">
         {currentItems.map((city, index) => (
+          // THESE PARAMS NEED TO BE CHANGED TO WHAT FIELDS ARE IN THE DATABASE
           <CitiesCard
             key={index}
+            id={city.id}
             img_src={city.img_src}
             name={city.name}
             population={city.population}
@@ -49,7 +84,7 @@ const Cities = () => {
       </div>
       <Pagination
         itemsPerPage={itemsPerPage}
-        totalItems={cities.length}
+        totalItems={apiCities.total_cities}
         paginate={paginate}
         currentPage={currentPage}
       />
