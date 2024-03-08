98443a3fb2695e5946d52c28de0f03dd24a8f690
Tyler Kubecka <tyandkub@gmail.com>
2024-03-07 23:49:29 -0600

    just use nav.link instead of link and then jest wont poop itself

--- front-end/src/components/cards/ExploreCard.jsx
+++ front-end/src/components/cards/ExploreCard.jsx
@@ -1,20 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Nav from 'react-bootstrap/Nav';
+
 
 const ExploreCard = (props) => {
   return (
     <div key={props.name} className="col-md-4 mb-4">
       <div className="card">
-        <Link to={`/${props.name.toLowerCase()}`} className="link">
+        <Nav.Link href={`/${props.name.toLowerCase()}`} class>
           <img
             className="card-img pt-2 px-2"
             src={props.img_src}
             style={{ objectFit: "contain", height: "200px" }}
             alt={`An icon representing ${props.name}.`}
           />
-        </Link>
+        </Nav.Link>
         <div className="card-body">
-          <Link to={`/${props.name.toLowerCase()}`} className="link">
+          <Nav.Link href={`/${props.name.toLowerCase()}`}>
             <h4
               className="card-title text-center text-decoration-none"
               style={{
@@ -24,7 +26,7 @@ const ExploreCard = (props) => {
             >
               <b>{props.name}</b>
             </h4>
-          </Link>
+          </Nav.Link>
         </div>
       </div>
     </div>
