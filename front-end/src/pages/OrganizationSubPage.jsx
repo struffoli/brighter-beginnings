9712aeccc9ac1352a0df4a67788aa6c607d536b2
Joshua Yu <joshty1215@gmail.com>
2024-04-02 19:43:08 -0500

    testing organizations map

--- front-end/src/pages/OrganizationSubPage.jsx
+++ front-end/src/pages/OrganizationSubPage.jsx
@@ -28,48 +28,96 @@ function OrganizationSubPage() {
     getScholarshipById(scholarship_id).then((data) => setApiScholarship(data));
     return (
       <div className="container" style={{ marginTop: "3%" }}>
-        <div className="row justify-content-center">
+        <div className="d-flex justify-content-around">
           <img
             className=""
             src={org.Organization.img_src}
-            style={{ height: "30%", width: "30%" }}
+            style={{ height: "29.5rem", width: "50%", objectFit: "cover" }}
             alt="Org"
           />
-        </div>
-        <div className="">
-          <h5 className="" style={{ paddingBottom: "4px" }}>
-            {org.Organization.name}
-          </h5>
-          <p className="card-text">
-            <b>Org Number:</b> {org.Organization.id}
-          </p>
-          <p className="card-text">
-            <b>Email:</b> {org.Organization.email}
-          </p>
-          <p className="card-text">
-            <b>Phone:</b> {org.Organization.phone}
-          </p>
-          <p className="card-text">
-            <b>Organization Type:</b> {org.Organization.organization_type}
-          </p>
-          <p className="">
-            <b>Scholarship: </b>
-            <Link to={`/scholarships/schp?id=${org.Organization.scholarship_id}`}>
-              {org.Organization.scholarship.name}
-            </Link>
-          </p>
-          <p className="card-text">
-            <b>Cities: </b>
-          </p>
-          {scholarship ? scholarship.Scholarship.cities.map((city, index) => (
-            <p>
-              <NavLink to={`/cities/city?id=${city.id}`} key={index}>
-                {city.name}
-              </NavLink>
+          <div className="px-5 w-100">
+            <h3 className="" style={{ paddingBottom: "4px" }}>
+              <b>{org.Organization.name}</b>
+            </h3>
+            <p className="card-text">
+              <b>Org Number:</b> {org.Organization.id}
             </p>
-          )) : <p>Loading...</p>}
+            <p className="card-text">
+              <b>Email:</b> {org.Organization.email}
+            </p>
+            <p className="card-text">
+              <b>Phone:</b> {org.Organization.phone}
+            </p>
+            <p className="card-text">
+              <b>Organization Type:</b> {org.Organization.organization_type}
+            </p>
+            <iframe
+              title="google map of this page's organization"
+              className="my-3"
+              width="100%"
+              height="53%"
+              style={{ border: 0 }}
+              loading="lazy"
+              allowfullscreen
+              referrerpolicy="no-referrer-when-downgrade"
+              src={
+                "https://www.google.com/maps/embed/v1/place?key=AIzaSyCN-0OitT1TAcITWdYcLeHnqGgqbA9eriQ&q=" +
+                (org.Organization.location
+                  ? org.Organization.location
+                  : "United States")
+              }
+            ></iframe>
+            <p className="">
+              <b>Scholarship: </b>
+              <Link
+                to={`/scholarships/schp?id=${org.Organization.scholarship_id}`}
+              >
+                {org.Organization.scholarship.name}
+              </Link>
+            </p>
+            <p className="card-text">
+              <b>Cities: </b>
+            </p>
+            {scholarship ? (
+              scholarship.Scholarship.cities.map((city, index) => (
+                <p>
+                  <NavLink to={`/cities/city?id=${city.id}`} key={index}>
+                    {city.name}
+                  </NavLink>
+                </p>
+              ))
+            ) : (
+              <p>Loading...</p>
+            )}
+            <ul className="list-group list-group-flush"></ul>
+          </div>
         </div>
       </div>
+      // <div className="container" style={{ marginTop: "3%" }}>
+      //   <div className="row justify-content-center">
+      //
+      //   </div>
+      //   <div className="">
+      //
+      //
+      //     <p className="">
+      //       <b>Scholarship: </b>
+      //       <Link to={`/scholarships/schp?id=${org.Organization.scholarship_id}`}>
+      //         {org.Organization.scholarship.name}
+      //       </Link>
+      //     </p>
+      //     <p className="card-text">
+      //       <b>Cities: </b>
+      //     </p>
+      //     {scholarship ? scholarship.Scholarship.cities.map((city, index) => (
+      //       <p>
+      //         <NavLink to={`/cities/city?id=${city.id}`} key={index}>
+      //           {city.name}
+      //         </NavLink>
+      //       </p>
+      //     )) : <p>Loading...</p>}
+      //   </div>
+      // </div>
     );
   }
 }
