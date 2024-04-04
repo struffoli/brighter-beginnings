8c44d1db0acb78e544e8d4bde4948c91fd659a3a
YummierYum <tyandkub@gmail.com>
2024-04-04 16:43:00 -0500

    user story

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -19,16 +19,16 @@ function App() {
         <NavBar />
         <Routes>
           <Route path="/" element={<Home />}></Route>
-          <Route path="/about" element={<About />}></Route>
-          <Route path="/about/dev" element={<AboutSubPage />}></Route>
-          <Route path="/cities" element={<Cities />}></Route>
+          <Route path="/about/" element={<About />}></Route>
+          <Route path="/about/dev/" element={<AboutSubPage />}></Route>
+          <Route path="/cities/" element={<Cities />}></Route>
           {/* add subpages */}
-          <Route path="/cities/city" element={<CitiesSubPage />}></Route>
-          <Route path="/organizations" element={<Organizations />}></Route>
-          <Route path="/organizations/org"element={<OrganizationSubPage />}></Route>
-          <Route path="/scholarships" element={<Scholarships />}></Route>
-          <Route path="/scholarships/schp"element={<ScholarshipsSubPage />}></Route>
-          <Route path="/search" element={<SearchResultsPage />}></Route>
+          <Route path="/cities/city/" element={<CitiesSubPage />}></Route>
+          <Route path="/organizations/" element={<Organizations />}></Route>
+          <Route path="/organizations/org/"element={<OrganizationSubPage />}></Route>
+          <Route path="/scholarships/" element={<Scholarships />}></Route>
+          <Route path="/scholarships/schp/"element={<ScholarshipsSubPage />}></Route>
+          <Route path="/search/" element={<SearchResultsPage />}></Route>
         </Routes>
       </Router>
     </div>
